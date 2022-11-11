99b9498a4aecc6d3d8afc4a87a761f10162c2cdf
zhaoshuoLF <shuo.zhao@lotusflare.com>
2022-11-11 14:21:18 +0800

    代码优化

--- app/src/main/java/com/zhaoshuo/recode/ui/LoginFragment.kt
+++ app/src/main/java/com/zhaoshuo/recode/ui/LoginFragment.kt
@@ -17,7 +17,6 @@ import com.zhaoshuo.recode.databinding.FragmentLoginBinding
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
-
     private val viewModel: RecodesViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
